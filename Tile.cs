69326b5fb626e9bfe7502b79303ff45131384c1d
Assaj <assaj@cin.ufpe.br>
2019-02-28 10:20:41 -0300

    Matrix separated by classes in other files

---
+++ Tile.cs
@@ -0,0 +1,22 @@
+ using namespace title;
+ class Tile
+    {
+        int PosX;
+        int PosY;
+        public Tile(int i, int i2)
+        {
+            this.PosX = i;
+            this.PosY = i2;
+        }
+
+        public int getPosX()
+        {
+            return PosX;
+        }
+        public int getPosY()
+        {
+            return PosY;
+        }
+
+    }
+    
